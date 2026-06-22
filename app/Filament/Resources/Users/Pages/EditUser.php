62c7349edc09dc4413869a5a6f0fc3ce641d4345
Mehdi Jalali <mehdi.jalalii03@gmail.com>
2026-06-22 12:35:20 +0330

    feat: add address tabs to user view and edit pages in Filament

--- app/Filament/Resources/Users/Pages/EditUser.php
+++ app/Filament/Resources/Users/Pages/EditUser.php
@@ -2,15 +2,99 @@
 
 namespace App\Filament\Resources\Users\Pages;
 
+use App\Filament\Resources\Users\Schemas\UserForm;
 use App\Filament\Resources\Users\UserResource;
+use App\Models\City;
 use Filament\Actions\DeleteAction;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Schemas\Components\Grid;
+use Filament\Schemas\Components\Tabs;
+use Filament\Schemas\Components\Tabs\Tab;
+use Filament\Schemas\Schema;
 
 class EditUser extends EditRecord
 {
     protected static string $resource = UserResource::class;
 
     protected static ?string $title = 'ویرایش کاربر';
+
+    public function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Tabs::make('UserTabs')
+                    ->columnSpanFull()
+                    ->tabs([
+                        Tab::make('اطلاعات کاربر')
+                            ->icon('heroicon-o-user')
+                            ->schema(UserForm::sections()),
+
+                        Tab::make('آدرس‌ها')
+                            ->icon('heroicon-o-map-pin')
+                            ->schema([
+                                Repeater::make('addresses')
+                                    ->relationship('addresses')
+                                    ->label('آدرس‌ها')
+                                    ->columnSpanFull()
+                                    ->schema([
+                                        Grid::make(2)
+                                            ->schema([
+                                                TextInput::make('label')
+                                                    ->label('عنوان')
+                                                    ->required()
+                                                    ->maxLength(50),
+                                                TextInput::make('receiver_name')
+                                                    ->label('نام گیرنده')
+                                                    ->required()
+                                                    ->maxLength(100),
+                                                TextInput::make('receiver_phone')
+                                                    ->label('تلفن گیرنده')
+                                                    ->required()
+                                                    ->maxLength(20),
+                                                TextInput::make('receiver_national_code')
+                                                    ->label('کد ملی گیرنده')
+                                                    ->maxLength(10),
+                                                Select::make('province_id')
+                                                    ->label('استان')
+                                                    ->relationship('province', 'name')
+                                                    ->searchable()
+                                                    ->required()
+                                                    ->live()
+                                                    ->afterStateUpdated(fn ($set) => $set('city_id', null)),
+                                                Select::make('city_id')
+                                                    ->label('شهر')
+                                                    ->options(fn ($get) => City::where('province_id', $get('province_id'))->pluck('name', 'id'))
+                                                    ->searchable()
+                                                    ->required()
+                                                    ->live(),
+                                                TextInput::make('district')
+                                                    ->label('منطقه')
+                                                    ->maxLength(100),
+                                                TextInput::make('postal_code')
+                                                    ->label('کد پستی')
+                                                    ->required()
+                                                    ->maxLength(20),
+                                                Textarea::make('address_line')
+                                                    ->label('آدرس')
+                                                    ->required()
+                                                    ->columnSpanFull(),
+                                                Toggle::make('is_default')
+                                                    ->label('آدرس پیش‌فرض'),
+                                                Toggle::make('is_billing')
+                                                    ->label('آدرس صورت‌حساب'),
+                                            ]),
+                                    ])
+                                    ->defaultItems(0)
+                                    ->addActionLabel('افزودن آدرس جدید'),
+                            ]),
+                    ]),
+            ]);
+    }
 
     protected function getHeaderActions(): array
     {
--- app/Filament/Resources/Users/Pages/ViewUser.php
+++ app/Filament/Resources/Users/Pages/ViewUser.php
@@ -7,10 +7,13 @@ use Filament\Actions\DeleteAction;
 use Filament\Actions\ForceDeleteAction;
 use Filament\Actions\RestoreAction;
 use Filament\Infolists\Components\IconEntry;
+use Filament\Infolists\Components\RepeatableEntry;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Resources\Pages\ViewRecord;
 use Filament\Schemas\Components\Grid;
 use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Tabs;
+use Filament\Schemas\Components\Tabs\Tab;
 use Filament\Schemas\Schema;
 
 class ViewUser extends ViewRecord
@@ -23,42 +26,86 @@ class ViewUser extends ViewRecord
     {
         return $schema
             ->components([
-                Section::make('اطلاعات هویتی')
-                    ->icon('heroicon-o-user')
-                    ->schema([
-                        Grid::make(3)
+                Tabs::make('UserTabs')
+                    ->columnSpanFull()
+                    ->tabs([
+                        Tab::make('اطلاعات کاربر')
+                            ->icon('heroicon-o-user')
                             ->schema([
-                                TextEntry::make('first_name')
-                                    ->label('نام'),
-                                TextEntry::make('last_name')
-                                    ->label('نام خانوادگی'),
-                                TextEntry::make('birth_date')
-                                    ->label('تاریخ تولد')
-                                    ->date('Y/m/d'),
+                                Section::make('اطلاعات هویتی')
+                                    ->icon('heroicon-o-user')
+                                    ->schema([
+                                        Grid::make(3)
+                                            ->schema([
+                                                TextEntry::make('first_name')
+                                                    ->label('نام'),
+                                                TextEntry::make('last_name')
+                                                    ->label('نام خانوادگی'),
+                                                TextEntry::make('birth_date')
+                                                    ->label('تاریخ تولد')
+                                                    ->jalaliDate('Y/m/d'),
+                                            ]),
+
+                                        Grid::make(3)
+                                            ->schema([
+                                                TextEntry::make('national_code')
+                                                    ->label('کد ملی'),
+                                                TextEntry::make('phone')
+                                                    ->label('موبایل'),
+                                                IconEntry::make('shahkar_verified')
+                                                    ->label('احراز هویت شاهکار')
+                                                    ->boolean(),
+                                                TextEntry::make('email')
+                                                    ->default('-')
+                                                    ->label('ایمیل'),
+
+                                                TextEntry::make('created_at')
+                                                    ->label('تاریخ ثبت‌نام')
+                                                    ->jalaliDateTime('H:i Y/m/d'),
+                                                TextEntry::make('updated_at')
+                                                    ->label('آخرین بروزرسانی')
+                                                    ->jalaliDateTime('H:i Y/m/d'),
+                                            ]),
+                                    ])
+                                    ->columnSpanFull(),
                             ]),
 
-                        Grid::make(3)
+                        Tab::make('آدرس‌ها')
+                            ->icon('heroicon-o-map-pin')
                             ->schema([
-                                TextEntry::make('national_code')
-                                    ->label('کد ملی'),
-                                TextEntry::make('phone')
-                                    ->label('موبایل'),
-                                IconEntry::make('shahkar_verified')
-                                    ->label('احراز هویت شاهکار')
-                                    ->boolean(),
-                                TextEntry::make('email')
-                                    ->default('-')
-                                    ->label('ایمیل'),
-
-                                TextEntry::make('created_at')
-                                    ->label('تاریخ ثبت‌نام')
-                                    ->jalaliDateTime('H:i Y/m/d'),
-                                TextEntry::make('updated_at')
-                                    ->label('آخرین بروزرسانی')
-                                    ->jalaliDateTime('H:i Y/m/d'),
+                                RepeatableEntry::make('addresses')
+                                    ->schema([
+                                        TextEntry::make('label')
+                                            ->label('عنوان'),
+                                        TextEntry::make('receiver_name')
+                                            ->label('گیرنده'),
+                                        TextEntry::make('receiver_phone')
+                                            ->label('تلفن'),
+                                        TextEntry::make('receiver_national_code')
+                                            ->label('کد ملی'),
+                                        TextEntry::make('province.name')
+                                            ->label('استان'),
+                                        TextEntry::make('city.name')
+                                            ->label('شهر'),
+                                        TextEntry::make('district')
+                                            ->label('منطقه'),
+                                        TextEntry::make('postal_code')
+                                            ->label('کد پستی'),
+                                        TextEntry::make('address_line')
+                                            ->label('آدرس')
+                                            ->columnSpanFull(),
+                                        IconEntry::make('is_default')
+                                            ->label('پیش‌فرض')
+                                            ->boolean(),
+                                        IconEntry::make('is_billing')
+                                            ->label('صورت‌حساب')
+                                            ->boolean(),
+                                    ])
+                                    ->columns(2)
+                                    ->grid(2)
+                                    ->label('آدرس‌ها'),
                             ]),
-                    ])
-                    ->columnSpanFull(),
+                    ]),
             ]);
     }
 
