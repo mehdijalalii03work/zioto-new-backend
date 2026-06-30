cf0e56cfee3e2fff84695ba51a7d2242731e077e
Mehdi Jalali <mehdi.jalalii03@gmail.com>
2026-06-30 11:21:10 +0330

    fix: redirect root URL to Filament login instead of missing landing views

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,5 @@
 <?php
 
-use App\Http\Controllers\LandingController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [LandingController::class, 'home'])->name('landing.home');
-Route::get('/about', [LandingController::class, 'about'])->name('landing.about');
-Route::get('/products/{slug}', [LandingController::class, 'product'])->name('landing.product');
-Route::get('/cart', [LandingController::class, 'cart'])->name('landing.cart');
-Route::get('/checkout', [LandingController::class, 'checkout'])->name('landing.checkout');
-Route::get('/login', [LandingController::class, 'login'])->name('landing.login');
-Route::get('/profile', [LandingController::class, 'profile'])->name('landing.profile');
-Route::get('/success', [LandingController::class, 'success'])->name('landing.success');
+Route::get('/', fn () => redirect()->route('filament.admin.auth.login'));
