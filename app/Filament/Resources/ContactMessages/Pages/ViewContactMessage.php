98eb26eaafdbc856fef9e5aa82e2782c6305ee1e
Mehdi Jalali <mehdi.jalalii03@gmail.com>
2026-07-11 12:25:25 +0330

    fix: use Filament\Schemas\Components\Section for v5

--- app/Filament/Resources/ContactMessages/Pages/ViewContactMessage.php
+++ app/Filament/Resources/ContactMessages/Pages/ViewContactMessage.php
@@ -4,9 +4,9 @@ namespace App\Filament\Resources\ContactMessages\Pages;
 
 use App\Filament\Resources\ContactMessages\ContactMessageResource;
 use Filament\Actions\Action;
-use Filament\Infolists\Components\Section;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Resources\Pages\ViewRecord;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class ViewContactMessage extends ViewRecord
