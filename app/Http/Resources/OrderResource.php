6260663e64d83dae067e4d4e7432c2228fdbef8a
Mehdi Jalali <mehdi.jalalii03@gmail.com>
2026-07-22 17:34:31 +0330

    feat: add installment_fee to OrderResource response

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -9,6 +9,8 @@ class OrderResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
+        $notesData = json_decode($this->notes, true) ?? [];
+
         return [
             'id' => $this->id,
             'order_number' => $this->order_number,
@@ -17,6 +19,7 @@ class OrderResource extends JsonResource
             'payment_method' => $this->payment_method,
             'payment_status' => $this->payment_status,
             'cancel_reason' => $this->cancel_reason,
+            'installment_fee' => $notesData['installment_fee'] ?? null,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
             'items' => OrderItemResource::collection($this->whenLoaded('items')),
