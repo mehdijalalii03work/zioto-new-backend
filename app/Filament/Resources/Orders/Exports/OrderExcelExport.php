71188464e5f9c983687676c6bc36c9e5404912de
Mehdi Jalali <mehdi.jalalii03@gmail.com>
2026-08-10 15:32:01 +0330

    fix excel

--- app/Filament/Resources/Orders/Exports/OrderExcelExport.php
+++ app/Filament/Resources/Orders/Exports/OrderExcelExport.php
@@ -347,8 +347,8 @@ class OrderExcelExport
             $c = $sheet->getCell("E$r10");
             $c->setValue("=ROUND(INDEX('خروجی اکسل سایت'!A:X,G$r10,15)/10000000,0)");
             $c->getStyle()->applyFromArray([
-                'font' => ['name' => 'B Titr', 'size' => 20, 'bold' => true, 'color' => ['rgb' => 'FF0000']],
-                'fill' => ['fillType' => Fill::FILL_SOLID, 'color' => ['rgb' => 'FFFFFF']],
+                'font' => ['name' => 'B Titr', 'size' => 20, 'bold' => true, 'color' => ['rgb' => 'FFFFFF']],
+                'fill' => ['fillType' => Fill::FILL_SOLID, 'color' => ['rgb' => 'FFFF00']],
                 'alignment' => ['horizontal' => 'center', 'vertical' => 'center'],
             ]);
 
